b38303c9360ae5d89dd14cae160582f8910317e5
Oluwaseyi-vibex <oluseyiwmwm@gmail.com>
2026-06-27 11:46:12 +0100

    feat: dashboard stats card

---
+++ src/components/StatsCard/StatsCard.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import './StatsCard.scss'
+
+type Props = {
+    src: string,
+    title: string,
+    number: number,
+}
+
+const StatsCard = (props: Props) => {
+    return (
+        <div className="stats-card">
+            <img src={props.src} width={40} height={40} alt={props.title} />
+            <h3>{props.title}</h3>
+            <p>{props.number}</p>
+        </div>
+    )
+}
+
+export default StatsCard
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,9 +1,21 @@
+import StatsCard from "../../components/StatsCard/StatsCard";
+import UsersIcon from '../../assets/dashboard/users-stats-card-.svg'
+
+
 import "./Dashboard.scss";
 
 const Dashboard = () => {
     return (
         <div className="dashboard-page">
-            <h1>Dashboard</h1>
+            <h1>Users</h1>
+
+
+            <div className="stats-cards-container">
+                <StatsCard src={UsersIcon} title="USERS" number={1000} />
+                <StatsCard src={UsersIcon} title="ACTIVE USERS" number={1000} />
+                <StatsCard src={UsersIcon} title="USERS WITH LOANS" number={1000} />
+                <StatsCard src={UsersIcon} title="USERS WITH SAVINGS" number={1000} />
+            </div>
         </div>
     )
 }
